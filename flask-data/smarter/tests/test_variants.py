3f0b4d76fcb04d13089b7eac87b60deed936158b
Paolo Cozzi <bunop@libero.it>
2021-06-22 14:51:11 +0200

    :white_check_mark: fix tests with date objects

--- flask-data/smarter/tests/test_variants.py
+++ flask-data/smarter/tests/test_variants.py
@@ -9,25 +9,48 @@ Created on Mon Jun 21 15:53:51 2021
 import json
 import pathlib
 
+from dateutil.parser import parse as parse_date
+from bson import json_util
+
 from .base import BaseCase, AuthMixin
 
 FIXTURES_DIR = pathlib.Path(__file__).parent / "fixtures"
 
 
-class VariantSheepTest(AuthMixin, BaseCase):
-    fixtures = [
-        'user',
-        'variantSheep'
-    ]
-
-    test_endpoint = '/api/variants/sheep/60ca279a8025a403796f644a'
+class DateMixin():
+    """Solve issues with dates from mongodb data"""
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
-        with open(f"{FIXTURES_DIR}/variantSheep.json") as handle:
-            cls.data = json.load(handle)[0]
+        with open(cls.data_file) as handle:
+            data = json.load(handle)
+
+        for i, record in enumerate(data):
+            # fix date in locations
+            for j, location in enumerate(record['locations']):
+                if 'date' in location:
+                    location['date'] = parse_date(location['date']['$date'])
+                    record['locations'][j] = location
+
+            data[i] = record
+
+        # dump data like mongodb does
+        tmp = json.dumps(data, default=json_util.default)
+
+        # tracking data
+        cls.data = json.loads(tmp)
+
+
+class VariantSheepTest(DateMixin, AuthMixin, BaseCase):
+    fixtures = [
+        'user',
+        'variantSheep'
+    ]
+
+    data_file = f"{FIXTURES_DIR}/variantSheep.json"
+    test_endpoint = '/api/variants/sheep/60ca279a8025a403796f644a'
 
     def test_get_variant(self):
         response = self.client.get(
@@ -38,23 +61,17 @@ class VariantSheepTest(AuthMixin, BaseCase):
         test = response.json
 
         self.assertIsInstance(test, dict)
-        self.assertEqual(test, self.data)
+        self.assertEqual(test, self.data[0])
 
 
-class VariantSheepListTest(AuthMixin, BaseCase):
+class VariantSheepListTest(DateMixin, AuthMixin, BaseCase):
     fixtures = [
         'user',
         'variantSheep'
     ]
 
+    data_file = f"{FIXTURES_DIR}/variantSheep.json"
     test_endpoint = '/api/variants/sheep'
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        with open(f"{FIXTURES_DIR}/variantSheep.json") as handle:
-            cls.data = json.load(handle)
 
     def test_get_variants(self):
         response = self.client.get(
@@ -71,20 +88,14 @@ class VariantSheepListTest(AuthMixin, BaseCase):
         self.assertEqual(response.status_code, 200)
 
 
-class VariantGoatTest(AuthMixin, BaseCase):
+class VariantGoatTest(DateMixin, AuthMixin, BaseCase):
     fixtures = [
         'user',
         'variantGoat'
     ]
 
+    data_file = f"{FIXTURES_DIR}/variantGoat.json"
     test_endpoint = '/api/variants/goat/60ca4dabd8f09cfd319da0f8'
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        with open(f"{FIXTURES_DIR}/variantGoat.json") as handle:
-            cls.data = json.load(handle)[0]
 
     def test_get_variant(self):
         response = self.client.get(
@@ -95,23 +106,17 @@ class VariantGoatTest(AuthMixin, BaseCase):
         test = response.json
 
         self.assertIsInstance(test, dict)
-        self.assertEqual(test, self.data)
+        self.assertEqual(test, self.data[0])
 
 
-class VariantGoatListTest(AuthMixin, BaseCase):
+class VariantGoatListTest(DateMixin, AuthMixin, BaseCase):
     fixtures = [
         'user',
         'variantGoat'
     ]
 
+    data_file = f"{FIXTURES_DIR}/variantGoat.json"
     test_endpoint = '/api/variants/goat'
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        with open(f"{FIXTURES_DIR}/variantGoat.json") as handle:
-            cls.data = json.load(handle)
 
     def test_get_variants(self):
         response = self.client.get(
