7ffe3cdf10ad18db552a9a8c7ad1d8bf87f4ef65
Paolo Cozzi <bunop@libero.it>
2022-04-29 18:14:21 +0200

    :white_check_mark: fix test info endpoint
    It's seems that dates are parsed correctly with the latest package versions

--- flask-data/smarter/tests/test_info.py
+++ flask-data/smarter/tests/test_info.py
@@ -2,8 +2,6 @@
 import json
 import pathlib
 
-from dateutil.parser import parse as parse_date
-
 from .base import BaseCase, AuthMixin
 
 FIXTURES_DIR = pathlib.Path(__file__).parent / "fixtures"
@@ -24,10 +22,6 @@ class TestSmarterInfo(AuthMixin, BaseCase):
 
         with open(f"{FIXTURES_DIR}/smarterInfo.json") as handle:
             cls.data = json.load(handle)[0]
-
-        cls.data['last_updated']['$date'] = parse_date(
-            cls.data['last_updated']['$date']
-        ).timestamp() * 1000
 
     def test_get_info(self):
         response = self.client.get(
